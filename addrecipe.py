d4e68803b3731ac308c66bddb6547dbe13c78cf8
yj-shrest <shresthayujal46@gmail.com>
2024-02-27 17:30:43 +0545

    add recipe 70%

--- addrecipe.py
+++ addrecipe.py
@@ -17,73 +17,104 @@ def reset(name,time,ingredients,steps):
 def clear_default_text(event, des):
     current_text = des.get("0.0","end")
     if current_text.strip() == "Add a short description":
-        print("del")
         des.delete(1.0, "end")
 def restore_default_text(event, des):
     current_text = des.get("0.0", "end")
     if not current_text.strip():  # Check if the text is empty
         des.insert("0.0", "Add a short description")
+def clear_default_text2(event, des):
+    current_text = des.get("0.0","end")
+    if current_text.strip() == "Write steps here........":
+        des.delete(1.0, "end")
+def restore_default_text2(event, des):
+    current_text = des.get("0.0", "end")
+    if not current_text.strip():  # Check if the text is empty
+        des.insert("0.0", "Write steps here........")
 class AddRecipeFrame(CTkFrame):
     def __init__(self, parent_frame, **kwargs):
         super().__init__(parent_frame, **kwargs)
-        self.configure(parent_frame,fg_color="#1B1C22",bg_color="#44454A")
+        self.configure(fg_color="#1B1C22",bg_color="#44454A")
        
         #label
-
+        ingredientslabel = CTkLabel(self,text="Ingredients required",text_color="#BFBBBB",bg_color="#1B1c22",font=("",14))
+        self.Difficultylabel = CTkLabel(self,text="Select Difficulty",text_color="#BFBBBB",bg_color="#1B1c22",font=("",14))
+        self.PrepTime = CTkLabel(self,text="Set Preparation Time",text_color="#BFBBBB",bg_color="#1B1c22",font=("",14))
+        self.hrlabel = CTkLabel(self,text="hr",text_color="#BFBBBB",bg_color="#1B1c22",font=("",20))
+        self.minlabel = CTkLabel(self,text="min",text_color="#BFBBBB",bg_color="#1B1c22",font=("",20))
+        self.stepslabel = CTkLabel(self,text="Detail Steps",text_color="#BFBBBB",bg_color="#1B1c22",font=("",16))
         font_size=10
        
         #entries
-        name=CTkEntry(self,placeholder_text="Add Title Here...",text_color="white",fg_color="#1B1C22",placeholder_text_color="#BFBBBB", border_width=0,font=("",30),width=400)
-        description=CTkTextbox(self,text_color="white",fg_color="#1B1C22", border_width=0,font=("",14),width=400)
+        name=CTkEntry(self,placeholder_text="Add Title Here...",text_color="white",fg_color="#1B1C22",placeholder_text_color="#BFBBBB", border_width=0,font=("",30),width=350)
+        description=CTkTextbox(self,text_color="white",fg_color="#1B1C22", border_width=0,font=("",14),width=350,height=90)
         description.insert("0.0","Add a short description")
         description.bind("<FocusIn>", lambda event, des=description: clear_default_text(event, des))
         description.bind("<FocusOut>", lambda event, des=description: restore_default_text(event, des))
-        time=CTkEntry(self,placeholder_text="Add Title Here...",text_color="white",fg_color="#1B1C22",placeholder_text_color="#BFBBBB", border_width=0,)
-        ingredients=CTkEntry(self,placeholder_text="Add Title Here...",text_color="white",fg_color="#1B1C22",placeholder_text_color="#BFBBBB", border_width=0,)
-        steps=CTkEntry(self,placeholder_text="Add Title Here...",text_color="white",fg_color="#1B1C22",placeholder_text_color="#BFBBBB", border_width=0,)
+        Steps=CTkTextbox(self,text_color="#BFBBBB",fg_color="#1B1C22", border_width=0,font=("",14),width=350,height=700)
+        Steps.insert("0.0","Write steps here........")
+        Steps.bind("<FocusIn>", lambda event, des=Steps: clear_default_text2(event, des))
+        Steps.bind("<FocusOut>", lambda event, des=Steps: restore_default_text2(event, des))
+        self.hr=CTkEntry(self,text_color="white",fg_color="#393A3B", border_width=0,font=("",20),width=60,corner_radius=15)
+        self.min=CTkEntry(self,text_color="white",fg_color="#393A3B", border_width=0,font=("",20),width=60,corner_radius=15)
+       
+
+        self.ingredient_entries = []  # List to hold ingredient entry fields
+
+        self.difficulty = CTkOptionMenu(self, values=["Easy", "Medium","Hard"],fg_color="#393A3B",button_color="#393A3B",dropdown_fg_color="#393A3B",button_hover_color="#393A3B",dropdown_hover_color="#393A3B",dropdown_text_color="white",text_color="white")
 
         #buttons
-        save_button = CTkButton(self,text="Save",fg_color="#F85656",corner_radius=30, command=lambda: adder(name.get(),description.get("0.0","end"), time.get(), ingredients.get(), steps.get()))
-        reset_button= CTkButton(self,text="Reset",fg_color="#5684F8",corner_radius=30,command=lambda: reset(name,time,ingredients,steps) )
+        save_button = CTkButton(self,text="Save",fg_color="#F85656",corner_radius=30, command=lambda: adder(name.get(),description.get("0.0","end"), ingredientName.get(), ingredients.get(), steps.get()))
+        reset_button= CTkButton(self,text="Reset",fg_color="#5684F8",corner_radius=30,command=lambda: reset(name,ingredientName,ingredients,steps) )
+        self.plus_button = CTkButton(self, text="+", fg_color="#1B1C22", text_color="#848484", font=("", 20), width=20, hover_color="#1B1C22", command=self.add_ingredient_entry)
+        self.add_ingredient_entry()  # Initially add one set of ingredient entry fields
 
+        # Layout
+        x = 50
+        x2 = 440
+        y = 50
+        y2=50
+        name.place(x=x, y=y)
+        y += 40
 
+        description.place(x=x, y=y)
+        y += 100
+
+        ingredientslabel.place(x=x + 10, y=y)
+        y += 40
+
+        self.stepslabel.place(x=x2,y=y2)
+        y2+= 30
+        Steps.place(x=x2,y=y2)
+
+    def add_ingredient_entry(self):
+        ingredient_name_entry = CTkEntry(self, placeholder_text="Ingredient Name", text_color="white", fg_color="#393A3B", placeholder_text_color="#848484", border_width=0, corner_radius=20, font=("", 16), width=200)
+        ingredient_qty_entry = CTkEntry(self, placeholder_text="Qty", text_color="white", fg_color="#393A3B", placeholder_text_color="#848484", border_width=0, corner_radius=20, font=("", 16), width=80)
+        self.ingredient_entries.append((ingredient_name_entry, ingredient_qty_entry))
+        start_y = 230
+        x = 50
+        for entry_pair in self.ingredient_entries:
+            entry_pair[0].place(x=x + 10, y=start_y)
+            entry_pair[1].place(x=x + 220, y=start_y)
+            start_y += 40
+        self.plus_button.place(x=x+100+230, y=start_y-40)
+        self.Difficultylabel.place(x=x+10,y=start_y)
+        self.difficulty.place(x=x+150,y=start_y)
+        start_y+=40
+        self.PrepTime.place(x=x+10,y=start_y)
+        start_y+=40
+        self.hr.place(x=x+10,y=start_y)
+        self.min.place(x=x+130,y=start_y)
+        self.hrlabel.place(x=x+80,y=start_y)
+        self.minlabel.place(x=x+200,y=start_y)
         
-        #put the things
-        after_label=20
-        after_entry=50
-        ypos=after_label
-        xpos=50
 
-        # name_label.place(x=xpos,y=ypos)
-        ypos=ypos+after_label
+# app = CTk()
+# app.geometry("1000x700")
+# add_recipe_frame = AddRecipeFrame(app,width=650)
+# app.mainloop()
 
-        name.place(x=xpos,y=ypos)
-        ypos=ypos+after_entry
 
-        description.place(x=xpos,y=ypos)
-        ypos=ypos+after_entry
 
-        # time_label.place(x=xpos,y=ypos)
-        ypos=ypos+after_label
-
-        time.place(x=xpos,y=ypos)
-        ypos=ypos+after_entry
-
-        # ingredients_label.place(x=xpos,y=ypos)        
-        ypos=ypos+after_label
-
-        ingredients.place(x=xpos,y=ypos)
-        ypos=ypos+after_entry
-
-        # steps_label.place(x=xpos,y=ypos)
-        ypos=ypos+after_label
-
-        steps.place(x=xpos,y=ypos)
-        ypos=ypos+after_entry
-
-       
-        reset_button.place(x=xpos,y=ypos)   
-        save_button.place(x=xpos+450,y=ypos)
    
 
 
--- main.py
+++ main.py
@@ -4,14 +4,26 @@ from dashboard import DashboardFrame
 from addrecipe import AddRecipeFrame
 app = CTk()
 app.geometry("1000x700")
-
+nav =1
 def callnav1():
+    global nav
     add_recipe_frame.grid_remove()
     dashboard_frame.initialize()
+    nav=1
+    placeitems()
 
 def callnav2():
+    global nav
     dashboard_frame.grid_remove()
     add_recipe_frame.grid(row=0,column=1,sticky="nsew")
+    nav=2
+    placeitems()
+
+def getnavcolor(n,nav):
+    if(n==nav): 
+        return "#F85656"
+    else:
+        return "transparent"
 app.grid_columnconfigure(0, weight=1)  
 app.grid_columnconfigure(1, weight=3)  
 app.grid_rowconfigure(0, weight=1)
@@ -37,20 +49,23 @@ logo_label.grid(row=0, column=0, sticky="nsew")
 # Navigation items
 navigation_items_frame = CTkFrame(navigation_frame, fg_color="transparent")
 navigation_items_frame.grid(row=1, column=0, sticky="nsew", pady=(0, 10))
+def placeitems():
+    for widget in navigation_items_frame.winfo_children():
+        widget.destroy()
+    button_dashboard = CTkButton(navigation_items_frame,hover=False,corner_radius=30,text="Dashboard", text_color="white", fg_color=getnavcolor(1,nav),command=callnav1)
+    button_dashboard.pack(pady=5)
 
-button_dashboard = CTkButton(navigation_items_frame,hover=False,corner_radius=30,text="Dashboard", text_color="white", fg_color="#F85656",command=callnav1)
-button_dashboard.pack(pady=5)
+    button_add_recipe = CTkButton(navigation_items_frame,hover=False,corner_radius=30, text="Add Recipe", text_color="white", fg_color=getnavcolor(2,nav),command=callnav2)
+    button_add_recipe.pack(pady=5)
 
-button_add_recipe = CTkButton(navigation_items_frame,hover=False, text="Add Recipe", text_color="white", fg_color="transparent",command=callnav2)
-button_add_recipe.pack(pady=5)
+    button_filter_sort = CTkButton(navigation_items_frame,hover=False,corner_radius=30, text="Filter Sort", text_color="white", fg_color=getnavcolor(3,nav))
+    button_filter_sort.pack(pady=5)
 
-button_filter_sort = CTkButton(navigation_items_frame,hover=False, text="Filter Sort", text_color="white", fg_color="transparent")
-button_filter_sort.pack(pady=5)
-
-button_favourites = CTkButton(navigation_items_frame,hover=False, text="Favourites", text_color="white", fg_color="transparent")
-button_favourites.pack(pady=5)
+    button_favourites = CTkButton(navigation_items_frame,hover=False,corner_radius=30, text="Favourites", text_color="white", fg_color=getnavcolor(4,nav))
+    button_favourites.pack(pady=5)
+placeitems()
 dashboard_frame = DashboardFrame(app,width=650)
-add_recipe_frame = AddRecipeFrame(app,width=650)
+add_recipe_frame = AddRecipeFrame(app,width=650,height= 1000)
 dashboard_frame.grid(row=0,column=1,sticky="nsew")
 app.mainloop()
 
